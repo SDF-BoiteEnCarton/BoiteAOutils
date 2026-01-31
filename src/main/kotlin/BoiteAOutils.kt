8812854d21df151f6564d6dd657f05d2f34cbb54
Clément Rostagni <clement.rostagni@efrei.net>
2026-01-31 02:51:29 +0100

    feat: adding hidden tag to replace item in hands of other players and fix build

--- src/main/kotlin/BoiteAOutils.kt
+++ src/main/kotlin/BoiteAOutils.kt
@@ -5,6 +5,7 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
 import me.clcondorcet.boiteaoutils.commands.BoiteCommand
 import me.clcondorcet.boiteaoutils.interactions.Distribute
+import me.clcondorcet.boiteaoutils.packets.HidingCardsPacketHandler
 
 class BoiteAOutils(init: JavaPluginInit) : JavaPlugin(init) {
 
@@ -20,5 +21,6 @@ class BoiteAOutils(init: JavaPluginInit) : JavaPlugin(init) {
         commandRegistry.registerCommand(BoiteCommand("boite", "Commandes de la boite à outils !"))
         getCodecRegistry(Interaction.CODEC)
             .register("boiteaoutils_distribute", Distribute::class.java, Distribute.CODEC)
+        HidingCardsPacketHandler().registerPacketCounters()
     }
 }
